aebc3b69c65b776584f915a99daaeb3aa4407bdc
eoftgge <sandaar.ykt@gmail.com>
2026-09-13 23:47:33 +0900

    refactor(gui/overlay/outline.rs): reorder and format `offsets` method

--- src/gui/overlay/outline.rs
+++ src/gui/overlay/outline.rs
@@ -7,24 +7,20 @@ pub struct TextOutline {
 }
 
 impl TextOutline {
-    pub(crate) fn offsets(&self) -> impl Iterator<Item = Vec2> + '_ {
-        const DIRS: [(f32, f32); 8] = [
-            (-1.0, -1.0),
-            (0.0, -1.0),
-            (1.0, -1.0),
-            (-1.0, 0.0),
-            (1.0, 0.0),
-            (-1.0, 1.0),
-            (0.0, 1.0),
-            (1.0, 1.0),
-        ];
-        DIRS.iter().map(move |&(x, y)| Vec2::new(x, y) * self.width)
-    }
-
     pub fn for_font_size(font_size: f32) -> Self {
         Self {
             color: Color32::from_black_alpha(220),
             width: (font_size / 14.0).clamp(1.0, 3.0),
         }
+    }
+
+    #[rustfmt::skip]
+    pub(crate) fn offsets(&self) -> impl Iterator<Item = Vec2> + '_ {
+        const DIRS: [(f32, f32); 8] = [
+            (-1.0, -1.0), (0.0, -1.0), (1.0, -1.0),
+            (-1.0,  0.0),              (1.0,  0.0),
+            (-1.0,  1.0), (0.0,  1.0), (1.0,  1.0),
+        ];
+        DIRS.iter().map(move |&(x, y)| Vec2::new(x, y) * self.width)
     }
 }
